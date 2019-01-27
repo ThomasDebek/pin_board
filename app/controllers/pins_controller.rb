76dacf7a70db1a4d1e0ecadb9eb7312889166960
tom.debek@gmail.com <tom.debek@gmail.com>
2019-01-27 22:15:35 +0100

    Added new method and views in my app

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,9 +1,15 @@
 class PinsController < ApplicationController
+  before_action :find_pin, only: [:show, :edit, :update, :destroy ]
+
   def index
+    @pins = Pin.all.order("created_at DESC")
   end
 
   def new
     @pin = Pin.new
+  end
+
+  def show
   end
 
   def create
@@ -22,5 +28,12 @@ class PinsController < ApplicationController
     params.require(:pin).permit(:title, :description)
   end
 
+  def find_pin
+    @pin = Pin.find(params[:id])
+  end
+
 
 end
+
+
+21:00
