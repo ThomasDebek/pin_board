1a5270c4036bfd9f38c8246ff55c8d0842803b86
tom.debek@gmail.com <tom.debek@gmail.com>
2019-02-18 21:16:41 +0100

    Added method to my controller pin

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,6 @@
 class PinsController < ApplicationController
   before_action :authenticate_user!, only: [:edit, :update, :destroy]
-  before_action :find_pin, only: [:show, :edit, :update, :destroy, :upvote ]
+  before_action :find_pin, only: [:show, :edit, :update, :destroy, :upvote]
 
   def index
     @pins = Pin.all.order("created_at DESC")
@@ -34,7 +34,7 @@ class PinsController < ApplicationController
 
   def upvote
     @pin.upvote_by current_user
-    redirect_to :back 
+    redirect_to root_path
   end
 
   def create
